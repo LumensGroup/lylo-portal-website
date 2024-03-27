5b9e241f5342bb52f7396a296da98f217e268d9a
zouzirui <zi.rui@lumens.sg>
2024-03-27 19:30:09 +0800

    redux存储一些数据

--- src/bases/store/reducers/count.ts
+++ src/bases/store/reducers/count.ts
@@ -3,11 +3,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 interface YourState {
   count: number;
   message: string;
+  searchData: object;
 }
 
 const initialState: YourState = {
   count: 0,
-  message: 'Hello, Redux!'
+  message: 'Hello, Redux!',
+  searchData: {},
 };
 
 const yourReducer = createSlice({
@@ -22,9 +24,12 @@ const yourReducer = createSlice({
     },
     setMessage: (state, action: PayloadAction<string>) => {
       state.message = action.payload;
-    }
+    },
+    setSearchData:(state, action: PayloadAction<object>) => {
+      state.searchData = action.payload;
+    },
   }
 });
 
-export const { increment, decrement, setMessage } = yourReducer.actions;
+export const { increment, decrement, setMessage, setSearchData } = yourReducer.actions;
 export default yourReducer.reducer;
