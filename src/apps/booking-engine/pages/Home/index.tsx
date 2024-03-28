fc253f93517a807717c8d761ab6b8d945e45a92d
Li Wang <liwang@LideMacBook-Pro.local>
2024-03-28 17:05:48 +0800

    add click for searchBar

--- src/apps/booking-engine/pages/Home/index.tsx
+++ src/apps/booking-engine/pages/Home/index.tsx
@@ -1,8 +1,5 @@
-import { Button, notification } from "antd";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import Icon from "../../../../bases/components/icon";
-import request from "../../../../bases/request";
 import { RootState } from "../../../../bases/store/reducers";
 import { useMediaQuery } from "react-responsive";
 import NewSearch from "@/bases/components/newSearch";
@@ -10,9 +7,9 @@ import PickUpEdit from "../SelectCar/components/pickup-edit";
 import MobileActionBar from "../SelectCar/components/action-bar";
 import { useState } from "react";
 import { FilterConditionState } from "../SelectCar/types";
+import { ROUTESMAP } from "../../routes";
 
 const HomePage = () => {
-  const { count } = useSelector((state: RootState) => state.count);
   const navigate = useNavigate();
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   const [filterCondition, setFilterCondition] = useState<FilterConditionState>({
@@ -39,29 +36,15 @@ const HomePage = () => {
     setEditPopupVisible(true);
   };
 
-  const handleClick = () => {
-    navigate("/list");
-  };
+  const handleOnSearchClick = () => {
+    navigate(ROUTESMAP.SelectCarPage);
+  }
 
-  const handleRequest = () => {
-    request
-      .get("/api/data")
-      .then((res) => {
-        console.log(res, "---");
-      })
-      .catch((e) => {
-        notification.error({
-          message: `Notification`,
-          description: e?.statusText,
-          placement: "topRight",
-        });
-      });
-  };
   return (
     <div>
       {!isMobile && (
         <div className="select-cars__search__area">
-          <NewSearch radiusType={false} shadowType={false} />
+          <NewSearch radiusType={false} shadowType={false} onSearchClick={handleOnSearchClick}/>
         </div>
       )}
       {isMobile && (
