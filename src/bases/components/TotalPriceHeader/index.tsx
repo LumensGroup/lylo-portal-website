b08dc80505e6e70a7a6185ae327dc3b2022d1d00
Li Wang <liwang@LideMacBook-Pro.local>
2024-03-28 12:32:57 +0800

    update UI

--- src/bases/components/TotalPriceHeader/index.tsx
+++ src/bases/components/TotalPriceHeader/index.tsx
@@ -1,8 +1,5 @@
 import { Button, Flex } from "antd";
-import { ReactNode, useCallback, useState } from "react";
-import Icon from "../icon";
 import "./index.scss";
-import { formatPrice } from "@/apps/booking-engine/pages/PaySuccess/components/PriceSummary/PriceSummaryItem";
 import { TCTip } from "@/apps/booking-engine/pages/Addons/components/summary";
 import MoneyComponent from "@/apps/booking-engine/pages/Addons/components/money";
 
@@ -11,18 +8,27 @@ interface TotalPriceHeaderProps {
 }
 
 const TotalPriceHeader: React.FC<TotalPriceHeaderProps> = ({ price }) => {
+  const priceStyle = {
+    fontSize: '19px',
+    fontWeight: 600,
+  };
+
+  const tipsStyle = {
+    fontWeight: 500,
+  };
+
   return (
     <div className="top_price_header">
       <h1>
         Total price
       </h1>
       <Flex className="header_price" align="center" vertical>
-        <div className="total-price"><MoneyComponent price={price} /></div>
+        <div className="total-price"><MoneyComponent price={price} style={priceStyle} /></div>
         <div className="total-price-unit">(incl. of GST)</div>
       </Flex>
 
-      <Button className="payment-checkout__button">Checkout</Button>
-      <TCTip />
+      <Button className="payment-checkout-button">Checkout</Button>
+      <TCTip style={tipsStyle}/>
     </div>
   )
 }
