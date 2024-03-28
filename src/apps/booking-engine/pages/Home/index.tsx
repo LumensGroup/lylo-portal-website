2b0eaec47e42f662d803d61eaf21718f7ec78dbe
Li Wang <liwang@LideMacBook-Pro.local>
2024-03-28 16:27:40 +0800

    add home page

--- src/apps/booking-engine/pages/Home/index.tsx
+++ src/apps/booking-engine/pages/Home/index.tsx
@@ -4,10 +4,40 @@ import { useNavigate } from "react-router-dom";
 import Icon from "../../../../bases/components/icon";
 import request from "../../../../bases/request";
 import { RootState } from "../../../../bases/store/reducers";
+import { useMediaQuery } from "react-responsive";
+import NewSearch from "@/bases/components/newSearch";
+import PickUpEdit from "../SelectCar/components/pickup-edit";
+import MobileActionBar from "../SelectCar/components/action-bar";
+import { useState } from "react";
+import { FilterConditionState } from "../SelectCar/types";
 
 const HomePage = () => {
   const { count } = useSelector((state: RootState) => state.count);
   const navigate = useNavigate();
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const [filterCondition, setFilterCondition] = useState<FilterConditionState>({
+    sale_price: [],
+    seating_category: [],
+    vehicle_type_category: [],
+  });
+  const [sortCondition, setSortCondition] = useState();
+  const [editPopupVisible, setEditPopupVisible] = useState(false);
+
+  const handleSort = (value: any) => setSortCondition(value);
+
+  const handleFilter = (keyName: string, value: any) => {
+    setFilterCondition((prevState) => {
+      return {
+        ...prevState,
+        [keyName]: value,
+      };
+    });
+  };
+
+
+  const handleEditClick = () => {
+    setEditPopupVisible(true);
+  };
 
   const handleClick = () => {
     navigate("/list");
@@ -29,15 +59,20 @@ const HomePage = () => {
   };
   return (
     <div>
-      路由跳转DEMO:
-      <Button onClick={handleClick}>click to reduxDemo 计数：{count}</Button>
-      <div>
-        发请求DEMO:
-        <Button onClick={handleRequest}>click to request</Button>
-      </div>
-      <div>
-        ICON demo <Icon source="ic_automatic" className="icon" />
-      </div>
+      {!isMobile && (
+        <div className="select-cars__search__area">
+          <NewSearch radiusType={false} shadowType={false} />
+        </div>
+      )}
+      {isMobile && (
+        <>
+          <PickUpEdit handleClick={handleEditClick} />
+          <MobileActionBar
+            handleFilter={handleFilter}
+            handleSort={handleSort}
+          />
+        </>
+      )}
     </div>
   );
 };
